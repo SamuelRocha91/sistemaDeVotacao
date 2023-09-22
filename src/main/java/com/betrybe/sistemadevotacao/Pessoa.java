54498d8555ba1071de01655920a2217dfae41f4a
SamuelRocha91 <samuel_sr@hotmail.com.br>
2023-09-22 14:20:07 -0300

    requisito 01

--- src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
+++ src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
@@ -1,5 +1,13 @@
 package com.betrybe.sistemadevotacao;
 
-public class Pessoa {
-  
+public abstract class Pessoa {
+  protected String nome;
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
 }
