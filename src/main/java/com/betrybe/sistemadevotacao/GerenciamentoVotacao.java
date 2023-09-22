57d7370df9092f53355126052564a29382bff994
SamuelRocha91 <samuel_sr@hotmail.com.br>
2023-09-22 15:28:25 -0300

    requisito 5

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -1,5 +1,42 @@
 package com.betrybe.sistemadevotacao;
 
-public class GerenciamentoVotacao {
+import java.util.ArrayList;
 
+public class GerenciamentoVotacao  implements  GerenciamentoVotacaoInterface {
+
+  private ArrayList<PessoaCandidata> pessoasCandidatas;
+
+  private ArrayList<PessoaEleitora> pessoasEleitoras;
+
+  private ArrayList<String> cpfsComputados;
+
+  @Override
+  public void cadastrarPessoaCandidata(String nome, int numero) {
+    PessoaCandidata novoCandidato = new PessoaCandidata(nome, numero);
+    if (this.pessoasCandidatas.contains(novoCandidato)) {
+      System.out.print("Número da pessoa candidata já utilizado!");
+    } else {
+      this.pessoasCandidatas.add(novoCandidato);
+    }
+  }
+
+  @Override
+  public void cadastrarPessoaEleitora(String nome, String cpf) {
+    PessoaEleitora novoEleitor = new PessoaEleitora(nome, cpf);
+    if (this.pessoasEleitoras.contains(novoEleitor)) {
+      System.out.print("Pessoa eleitora já cadastrada!");
+    } else {
+      this.pessoasEleitoras.add(novoEleitor);
+    }
+  }
+
+  @Override
+  public void votar(String cpfPessoaEleitora, int numeroPessoaCandidata) {
+
+  }
+
+  @Override
+  public void mostrarResultado() {
+
+  }
 }
