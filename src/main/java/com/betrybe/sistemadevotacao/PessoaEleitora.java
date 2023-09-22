6df3fb1b123a035e38ede33fa7573328efa19f71
SamuelRocha91 <samuel_sr@hotmail.com.br>
2023-09-22 15:05:45 -0300

    requisito 03

--- src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
@@ -1,5 +1,28 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaEleitora{
+public class PessoaEleitora extends Pessoa {
+  private String cpf;
 
+  public PessoaEleitora(String nome, String cpf) {
+    this.setCpf(cpf);
+    this.setNome(nome);
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
+
+  @Override
+  public String getNome() {
+    return nome;
+  }
+
+  @Override
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
 }
