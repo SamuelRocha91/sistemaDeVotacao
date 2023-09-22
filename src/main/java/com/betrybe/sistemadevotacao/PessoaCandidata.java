5987289905fc9a4b6636ccbeaef4cbc3cb42b580
SamuelRocha91 <samuel_sr@hotmail.com.br>
2023-09-22 14:59:40 -0300

    requisito 02

--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -1,5 +1,48 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaCandidata {
+public class PessoaCandidata extends Pessoa {
+  int numero;
+  int votos;
 
+  /**
+   * Cria a classe pessoa candidata e seus atributos e métodos.
+   * @param numero inteiro.
+   * @param nome  string.
+   */
+  public PessoaCandidata(String nome, int numero) {
+    this.setNome(nome);
+    this.numero = numero;
+    this.votos = 0;
+  }
+
+
+  public int getNumero() {
+    return numero;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public int getVotos() {
+    return votos;
+  }
+
+  public void receberVoto() {
+    this.votos += 1;
+  }
+
+  public void setVotos(int votos) {
+    this.votos = votos;
+  }
+
+  @Override
+  public String getNome() {
+    return nome;
+  }
+
+  @Override
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
 }
